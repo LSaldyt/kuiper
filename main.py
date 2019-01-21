b37df0310e7fcc6dbf7fe6a9ce1d5bd290e6df79
LSaldyt <lucassaldyt@gmail.com>
2019-01-21 01:27:49 -0700

    Adds SFML boiler

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-from direct.showbase.ShowBase import ShowBase
+import sfml as sf
 
-class MyApp(ShowBase):
+print(dir(sf.window))
+print(dir(sf.audio))
+print(dir(sf.graphics))
+print(dir(sf.network))
+print(dir(sf.system))
+1/0
 
-    def __init__(self):
-        ShowBase.__init__(self)
+def main():
+    window = sf.window(sf.VideoMode(640, 480),
+                             'Drawing an image with SFML')
+    window.framerate_limit = 60
+    running = True
+    texture = sf.Texture.load_from_file('python-logo.png')
+    sprite = sf.Sprite(texture)
 
-        # Load the environment model.
-        self.scene = self.loader.loadModel("models/environment")
-        # Reparent the model to render.
-        self.scene.reparentTo(self.render)
-        # Apply scale and position transforms on the model.
-        self.scene.setScale(0.25, 0.25, 0.25)
-        self.scene.setPos(-8, 42, 0)
+    while running:
+        for event in window.iter_events():
+            if event.type == sf.Event.CLOSED:
+                running = False
+
+        if sf.Keyboard.is_key_pressed(sf.Keyboard.RIGHT):
+            sprite.move(5, 0)
+        elif sf.Keyboard.is_key_pressed(sf.Keyboard.LEFT):
+            sprite.move(-5, 0)
+
+        window.clear(sf.Color.WHITE)
+        window.draw(sprite)
+        window.display()
+
+    window.close()
 
 
-app = MyApp()
-app.run()
+if __name__ == '__main__':
+    main()
